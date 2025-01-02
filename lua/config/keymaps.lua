442727b07771ae151fc535d2cbf98fcbcb8acaf7
ffyruh <78766072+ffyruh@users.noreply.github.com>
2025-01-02 14:57:36 -0500

    remap move lines up and down

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
--- remap hori split
+
+-- remap splits
 vim.keymap.del('n', '<leader>|')
 vim.keymap.set('n', '<leader>\\', '<C-W>v', { desc = 'Split Window Right', remap = true })
+
+-- unbind defaults for move up and down
+vim.keymap.del({ 'n', 'i', 'v' }, '<A-k>')
+vim.keymap.del({ 'n', 'i', 'v' }, '<A-j>')
+-- bind new up down in visual mode
+vim.keymap.set('v', '<S-j>', ":m '>+1<CR>gv=gv", { desc = 'Move lines down', silent = true })
+vim.keymap.set('v', '<S-k>', ":m '<-2<CR>gv=gv", { desc = 'Move lines up', silent = true })
